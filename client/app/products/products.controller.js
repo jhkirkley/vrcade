9624a68b8a2eb25e48f27f631b5a57ff57116f15
John Kirkley <john@johnkirkley.com>
2016-03-03 14:24:49 -0500

    checkout -page

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -107,7 +107,26 @@ angular.module('vrshopApp')
     };
 
     $scope.upload = uploadHander($scope, Upload, $timeout);
-  });
+  })
+  .controller('ProductCheckoutCtrl',
+  function($scope, $http, $state, ngCart){
+  $scope.errors = '';
+
+  $scope.paymentOptions = {
+    onPaymentMethodReceived: function(payload) {
+      angular.merge(payload, ngCart.toObject());
+      payload.total = payload.totalCost;
+      console.error(payload);
+      $http.post('/api/orders', payload)
+      .then(function success () {
+        ngCart.empty(true);
+        $state.go('products');
+      }, function error (res) {
+        $scope.errors = res;
+      });
+    }
+  };
+});
 
 
 
